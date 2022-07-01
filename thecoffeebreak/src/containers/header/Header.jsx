e550e915d46e4338bc412466fc23d41bd291e02b
Hugo Garelha <hugo_garelha_123@hotmail.com>
2022-07-01 14:52:43 +0100

    Header Base

--- thecoffeebreak/src/containers/header/Header.jsx
+++ thecoffeebreak/src/containers/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import logo from '../../assets/Logo_2.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header section_padding' id='Home'>
+      <div className='header-content'>
+        <h1 className='gradient_text'>Lorem ipsum dolor, sit amet consectetur adipisicing.</h1>
+        <div className='header-content_text'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae illum optio quos. Neque, voluptas est.</p>
+        </div>
+        <div className='header-image'>
+          <img src={logo} alt='The Coffee Break'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
